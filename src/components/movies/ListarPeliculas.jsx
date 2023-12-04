ceabd871cecd386a19c1f457191dcd5ebcc554d4
jaimezzapata <zapataval2304@gmail.com>
2023-12-03 23:20:22 -0500

    Se agregan estilos a las cards

--- src/components/movies/ListarPeliculas.jsx
+++ src/components/movies/ListarPeliculas.jsx
@@ -1,34 +1,35 @@
-import axios from 'axios'
-import Header from "../helpers/Header"
-import { useState } from 'react'
+import axios from "axios";
+import Header from "../helpers/Header";
+import { useState } from "react";
 
 const ListarPeliculas = () => {
-  const [peliculas, setPeliculas] = useState([])
+  const [peliculas, setPeliculas] = useState([]);
 
   const traerPeliculas = async () => {
-    let listadoPeliculas = await axios.get('http://localhost:3001/peliculas')
-    setPeliculas(listadoPeliculas.data)
-  }
+    let listadoPeliculas = await axios.get("http://localhost:3001/peliculas");
+    setPeliculas(listadoPeliculas.data);
+  };
+  
 
   return (
-    <div>
+    <div className="peliculas">
       <Header />
-      <h1>Listado peliculas</h1>
-      {
-        peliculas.map((pelicula)=>(
+      <section className="cards">
+        {peliculas.map((pelicula) => (
           <section>
             <p>Nombre: {pelicula.nombre}</p>
             <p>Calificacion: {pelicula.calificacion}</p>
             <p>Clasificacion: {pelicula.clasifiacion}</p>
             <p>Año: {pelicula.anio}</p>
             <p>Descripcion: {pelicula.descripcion}</p>
-
           </section>
-        ))
-      }
-      <button type='butoon' onClick={traerPeliculas}>Mostrar</button>
+        ))}
+      </section>
+      <button type="butoon" onClick={traerPeliculas}>
+        Mostrar
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default ListarPeliculas
+export default ListarPeliculas;
