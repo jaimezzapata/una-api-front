e07b06fc5e0fe050b40223a875906947ca259a14
jaimezzapata <zapataval2304@gmail.com>
2023-12-04 10:37:23 -0500

    Se agrega validaciones de sweetalert2

--- src/components/movies/CrearPelicula.jsx
+++ src/components/movies/CrearPelicula.jsx
@@ -1,12 +1,32 @@
 import { useState } from "react";
-
+import Swal from "sweetalert2";
 
 const CrearPelicula = () => {
-  const [nombre, setNombre] = useState('')
-  const [calificacion, setCalificacion] = useState('')
-  const [clasificacion, setClasificacion] = useState('')
-  const [anio, setAnio] = useState('')
-  const [descripcion, setDescripcion] = useState('')
+  const [nombre, setNombre] = useState("");
+  const [calificacion, setCalificacion] = useState("");
+  const [clasificacion, setClasificacion] = useState("");
+  const [anio, setAnio] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+
+  const agregarPelicula = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
 
   return (
     <div className="form-container">
@@ -14,25 +34,47 @@ const CrearPelicula = () => {
       <form action="#" method="post">
         <div className="form-group">
           <label htmlFor="nombre">Nombre:</label>
-          <input onChange={(e)=> setNombre(e.target.value)} type="text" required />
+          <input
+            onChange={(e) => setNombre(e.target.value)}
+            type="text"
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Calificacion</label>
-          <input onChange={(e)=> setCalificacion(e.target.value)} type="text" required />
+          <input
+            onChange={(e) => setCalificacion(e.target.value)}
+            type="text"
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Clasificación</label>
-          <input onChange={(e)=> setClasificacion(e.target.value)} type="text" required />
+          <input
+            onChange={(e) => setClasificacion(e.target.value)}
+            type="text"
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Año:</label>
-          <input onChange={(e)=> setAnio(e.target.value)} type="text" required />
+          <input
+            onChange={(e) => setAnio(e.target.value)}
+            type="text"
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Descripción:</label>
-          <input onChange={(e)=> setDescripcion(e.target.value)} type="text" required />
+          <input
+            onChange={(e) => setDescripcion(e.target.value)}
+            type="text"
+            required
+          />
         </div>
-        <button type="submit">Enviar</button>
+        <button onClick={agregarPelicula} type="button">
+          Enviar
+        </button>
       </form>
     </div>
   );
